17c0671bcb6fbec057ff8c3da8f989bc4c90ea3d
Reegan <reegank20@gmail.com>
2025-09-03 23:58:46 +0530

    Index

--- app/index.jsx
+++ app/index.jsx
@@ -4,13 +4,13 @@ import { useEffect } from "react";
 export default function Index() {
   useEffect(() => {
     setTimeout(() => {
-      router.push("./(screen)/Dashboard");
+      router.push("/(screen)/Dashboard");
     }, 500);
   }, []);
   return (
     <View className="flex-1 justify-end p-5">
       <TouchableOpacity
-        onPress={() => router.push("./(screen)/Dashboard")}
+        onPress={() => router.push("/Login")}
         className="h-12 px-6 rounded-xl mb-12 bg-black items-center justify-center self-stretch"
       >
         <Text className="text-white text-base font-semibold">Login</Text>
